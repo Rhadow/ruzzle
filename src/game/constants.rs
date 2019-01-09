f541051b786300850d009ef3bfa9fa9bddf095ab
Rhadow <howard3433@gmail.com>
2019-01-08 23:33:51 -0800

    Update grassland and wooden path sprite

--- src/game/constants.rs
+++ src/game/constants.rs
@@ -7,13 +7,13 @@ pub const CELL_SIZE: f64 = 64f64;
 pub const ASSET_SIZE: f64 = 16f64;
 
 // Terrains
-pub const GRASS_LAND_X_OFFSET: f64 = 5f64;
+pub const GRASS_LAND_X_OFFSET: f64 = 7f64;
 pub const GRASS_LAND_Y_OFFSET: f64 = 9f64;
-pub const GRASS_LAND_SIZE: f64 = 16f64;
+pub const GRASS_LAND_SIZE: f64 = 32f64;
 
-pub const WOODEN_PATH_X_OFFSET: f64 = 15f64;
-pub const WOODEN_PATH_Y_OFFSET: f64 = 18f64;
-pub const WOODEN_PATH_SIZE: f64 = 16f64;
+pub const WOODEN_PATH_X_OFFSET: f64 = 13f64;
+pub const WOODEN_PATH_Y_OFFSET: f64 = 15f64;
+pub const WOODEN_PATH_SIZE: f64 = 32f64;
 
 // Objects
 pub const TREE_X_OFFSET: f64 = 16f64;
